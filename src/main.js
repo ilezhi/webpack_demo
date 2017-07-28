7a18fe94aa089699e7aeeef9175e0b940ef83d21
weels <604403450@qq.com>
2017-07-28 10:31:10 +0800

    'entry'

--- src/main.js
+++ src/main.js
@@ -1,9 +1,1 @@
-import a from './components/a';
-import './assets/css/site.css';
-
-(function() {
-    var h = document.createElement('h1');
-    h.innerText = '入口文件';
-    document.getElementById('container').appendChild(h);
-    a();
-})();
+document.getElementById('box').innerHTML = '<h4>webpack demo</h4>';
